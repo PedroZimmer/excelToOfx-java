48e3ec9138d70c49fdb1b928496c95f50929dc7a
Pedro Eccel Zimmermann <pedroez@weg.net>
2025-05-15 09:00:53 -0300

    first commit

---
+++ src/main/java/com/pedrozimmer/excelToOfx/ExcelToOfxApplication.java
@@ -0,0 +1,13 @@
+package com.pedrozimmer.excelToOfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelToOfxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelToOfxApplication.class, args);
+	}
+
+}
